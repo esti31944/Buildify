caca14c19b29c5c79dc856294c44220617eefe96
esti31944 <esti31944@gmail.com>
2025-11-09 13:44:20 +0200

    add the issues

--- SERVER/routes/configRoutes.js
+++ SERVER/routes/configRoutes.js
@@ -1,9 +1,9 @@
 const indexR = require("./index");
 const usersR = require("./userRoutes");
 // const paymentsR = require("./payments");
-// const issuesR = require("./issues");
-// const roomsR = require("./rooms");
-// const reservationsR = require("./reservations");
+const issuesR = require("./issueRoutes");
+const roomsR = require("./roomRoutes");
+const reservationsR = require("./reservationRoutes");
 // const noticesR = require("./notices");
 // const notificationsR = require("./notifications");
 
@@ -11,9 +11,9 @@ exports.routesInit = (app) => {
   app.use("/", indexR);
   app.use("/users", usersR);
 //   app.use("/payments", paymentsR);
-//   app.use("/issues", issuesR);
-//   app.use("/rooms", roomsR);
-//   app.use("/reservations", reservationsR);
+  app.use("/issues", issuesR);
+  app.use("/rooms", roomsR);
+  app.use("/reservations", reservationsR);
 //   app.use("/notices", noticesR);
 //   app.use("/notifications", notificationsR);
 };
